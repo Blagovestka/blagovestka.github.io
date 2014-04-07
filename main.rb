6d93a6b6b550cebe3d8c5e8f39f9efb88e90515b
Blagovestka <blagovestka@spreedinc.com>
2014-04-07 20:59:36 +0300

    generating main home page

--- main.rb
+++ main.rb
@@ -1,13 +1,47 @@
 # encoding: UTF-8
-main1='<html>
-	<head>
-		<title>Блага</title>
-		<link rel="icon" type="image/png" href="../images/favicon.png">
-		<nav>
-			<a style="color:#DEDED9;" href="../Blag.html">Блогът на Блага</a> 
-			<a style="color:#DEDED9;" href="../en/index.html">Еnglish</a>
-		</nav>
-	</head>
+home_page = '<!DOCTYPE html>
+<html>
+<head>
+	<title>Благи Странички</title>
+	<meta http-equiv="refresh" content="3; URL=bg/index.html">
+	<meta name="keywords" content="automatic redirection">
+	<link rel="icon" type="image/png" href="images/favicon.png">
+</head>
+
+<body style="text-aligh:center; 
+font-family:Comic Sans MS; 
+color:#DEDED9;
+background-image:url("images/background.jpg");
+background-size:cover;
+background-repeat:REPEAT-SETTINGS;">
+
+	<h2>
+		Здравейте!
+	</h2><br>
+	<h2>
+		Hello!
+	</h2>
+	<h5>
+		Ще бъдете прехвърлени към българската версия на сайта след няколко секунди.
+	</h5>
+
+</body>
+</html>'
+
+File.write "C:\\Users\\Girls\\Documents\\GitHub\\blagovestka.github.io\\index.html", home_page
+
+html_head ='<!DOCTYPE html>
+<html>
+<head>
+	<title>'
+bg_title = 'Благи Странички'
+html_end_head = '</title>
+	<link rel="icon" type="image/png" href="../images/favicon.png">
+	<nav>
+		<a style="color:#DEDED9;" href="../Blag.html">Блогът на Блага</a> 
+		<a style="color:#DEDED9;" href="../en/index.html">Еnglish</a>
+	</nav>
+</head>
 
 	<body style="text-aligh:center; 
 	font-family:Comic Sans MS; 
@@ -18,15 +52,15 @@ main1='<html>
 
 		<h1>'
 
-		main3='</h1>
+		end_text = '</h1>
 
 	</body>
 </html>'
 
 bg_head = "Проверка"
 en_head = "Testing Out"
-bg_whole = main1 + bg_head + main3
-en_whole = main1 + en_head + main3
+bg_whole = html_head + bg_title + html_end_head + bg_head + end_text
+en_whole = html_head + bg_title + html_end_head + en_head + end_text
 
-File.write "C:\\Users\\Girls\\Documents\\GitHub\\blagovestka.github.io\\bg.html", bg_whole
-File.write "C:\\Users\\Girls\\Documents\\GitHub\\blagovestka.github.io\\en.html", en_whole
+File.write "C:\\Users\\Girls\\Documents\\GitHub\\blagovestka.github.io\\bg\\bg.html", bg_whole
+File.write "C:\\Users\\Girls\\Documents\\GitHub\\blagovestka.github.io\\en\\en.html", en_whole
